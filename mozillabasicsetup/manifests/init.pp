e75207e5c62fb971f931ff5ca22969ffc4dc9891
Nico Nilsson <a1500946@myy.haaga-helia.fi>
2016-12-13 16:35:27 +0200

    Toka push, viimeistely

--- mozillabasicsetup/manifests/init.pp
+++ mozillabasicsetup/manifests/init.pp
@@ -5,4 +5,13 @@ class mozillabasicsetup {
 	file {"/etc/firefox/syspref.js":
 		content => template('mozillabasicsetup/syspref.js'),
 	}
+	Exec {
+		path => '/bin/:/usr/bin/:/sbin/:/usr/sbin/',
+	}
+	exec { 'ufw80':
+		command => 'ufw allow 80',
+	}
+	exec { 'ufw443':
+		command => 'ufw allow 443',
+	}
 }
